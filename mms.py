e234df23cabe6172d65e042856bc46607871b06a
Peter-Gess <gesspeter@gmail.com>
2019-06-20 15:46:12 -0500

    Update mms.py

--- mms.py
+++ mms.py
@@ -1,13 +1,15 @@
+# Sending a picture message using the Vivial Connect Python SDK
+
 from vivialconnect import Resource, Message, Attachment
 
 # Configure SDK
-Resource.api_key = "MTKOVFNVYKUOPQFD9LZTLYQZRJAGW4VUJLV"
-Resource.api_secret = "kuvwFXgKXson8JYomc5OPurrEoa96cWfTlOg7PXob8mvEeRh"
-Resource.api_account_id = "10193"
+Resource.api_key = ""
+Resource.api_secret = ""
+Resource.api_account_id = ""
 
 # Set up Message object
-from_number = "+16124282883"
-to_number = "+13202930706"
+from_number = ""
+to_number = ""
 
 message = Message()
 message.media_urls = ['http://prod.static.vikings.clubs.nfl.com/nfl-assets/img/gbl-ico-team/MIN/logos/home/large.png']
